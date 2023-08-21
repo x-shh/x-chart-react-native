e44030b5c96292cc255a0b164878749d3b3592be
Sahan <sahan@Sahan-Herath-MacBook-Pro.local>
2023-08-21 16:35:44 +0530

    added new files

---
+++ react-native-chart/chart/itx-x-chart-template-data-provider.js
@@ -0,0 +1,22 @@
+(function (infChart, $, angular) {
+
+    infChart.xinTemplatesDataProvider = function () {
+        infChart.templatesDataProvider.apply(this, arguments);
+
+        var injector = angular.element('body > [ng-controller]:first').injector();
+
+        this.chartUtilService = injector.get("chartUtilService");
+    };
+
+    infChart.util.extend(infChart.templatesDataProvider, infChart.xinTemplatesDataProvider);
+
+    infChart.xinTemplatesDataProvider.prototype.saveChartTemplates = function (type, templates) {
+        this.chartUtilService.saveChartTemplates(type, templates);
+    };
+
+    infChart.xinTemplatesDataProvider.prototype.getChartTemplates = function(type, onSuccess) {
+        this.chartUtilService.getChartTemplates(type).then(function(data){
+            onSuccess(data);
+        });
+    };
+})(infChart, jQuery, angular);
